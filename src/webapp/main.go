23ab285c00b2e4964a3d5ad74a17558ff745e0f2
Tarun Vashisth <tarun.vashisth@target.com>
2018-01-20 01:50:49 +0530

    Handle root path and serve static folder

--- src/webapp/main.go
+++ src/webapp/main.go
@@ -9,9 +9,14 @@ import (
 
 func main() {
 
-	// Serve public folder
-	publicDir := strings.Join([]string{"src", "webapp", "public"}, string(os.PathSeparator))
-	http.Handle("/", http.FileServer(http.Dir(publicDir)))
+	//Serve static folder
+	staticDir := strings.Join([]string{"src", "webapp", "static"}, string(os.PathSeparator))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
+
+	//Root handler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("The server is up and running!!"))
+	})
 
 	fmt.Println("Server listening on 8080...")
 	http.ListenAndServe(":8080", nil)
