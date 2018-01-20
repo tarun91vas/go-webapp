704cfce06504389a3d885d4c47b738a9a75b5640
Tarun Vashisth <tarun.vashisth@target.com>
2018-01-20 17:21:59 +0530

    Add viewHandler using templates

--- src/webapp/main.go
+++ src/webapp/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 	"strings"
 )
 
 func main() {
+
+	// Handle view task
+	http.HandleFunc("/view/", viewHandler)
 
 	//Serve static folder
 	staticDir := strings.Join([]string{"src", "webapp", "static"}, string(os.PathSeparator))
@@ -20,4 +24,18 @@ func main() {
 
 	fmt.Println("Server listening on 8080...")
 	http.ListenAndServe(":8080", nil)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	reqPath := r.URL.Path[len("/view/"):]
+	if strings.EqualFold(reqPath, "") {
+		reqPath = "index.html"
+	}
+	templatePath := strings.Join([]string{"src", "webapp", "templates", reqPath}, string(os.PathSeparator))
+	t, err := template.ParseFiles(templatePath)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	t.Execute(w, nil)
 }
